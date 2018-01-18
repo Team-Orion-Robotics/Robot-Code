9ee0edcaaf804d734c462cebd05aa9659a2911ad
Daniel LANGRIDGE <8DLA1005@barton.ac.uk>
2018-01-18 13:21:10 +0000

    Added movement Code

--- Main.py
+++ Main.py
@@ -7,9 +7,13 @@ r = Robot()
 
 tokens = r.camera.see()
 
+x = 1
+
+while (x =- 1):
+    Move_Forward(100)
+
 def Move_forward(Speed)
-
-
-
+	MOTOR_LEFT.power = Speed
+    MOTOR_RIGHT.power = Speed
 
 #pip install git+https://github.com/sourcebots/robot-ap
